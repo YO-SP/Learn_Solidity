91fa3055afff1941e10f60d3e6e7264e027e1f2b
Yakobus Octavianus Sucahyo Putra <yakobusavin2004@gmail.com>
2026-02-04 11:23:27 +0700

    Menambah materi function

--- Function.sol
+++ Function.sol
@@ -3,5 +3,26 @@ pragma solidity ^0.8.26;
 
 contract FunctionExample {
 
+// Fungsi TANPA return
+// function namaFungsi visibility mutability modifier(optional)
+
+// Fungsi DENGAN return
+// function namaFungsi visibility mutability modifier(optional) returns (tipeData)
+
+uint256 public number; 
+
+function setNumber(uint _num) external {
+    number = _num;
+}
+
+function getNumber() external view returns (uint) {
+    return number;
+}
+
+function sum(uint a, uint b) public pure returns (uint) {
+    return a + b;
+}
+
+
 }
 
